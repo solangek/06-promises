8ff938e7c3e3366bbfc70db3b00e004a4edac296
Sol <sodanabe@gmail.com>
2022-11-27 15:01:48 +0200

    2022

--- chaining1.js
+++ chaining1.js
@@ -1,6 +1,34 @@
 const fetchData = () => {
+    return new Promise((resolve, reject) => {
+        //console.log("building Promise");
+        setTimeout(() => {
+            resolve('Promise is Done!');
+        }, 1000);
+    });
+};
+
+console.log('Hello!');
+
+setTimeout(() => {
+    //console.log('Timer is done!');
+    fetchData()
+        .then(text => {
+            //console.log("1:");
+            console.log(text);
+            return fetchData();
+        })
+        .then(text2 => {
+            //console.log("2:");
+            console.log(text2);
+        });
+}, 2000);
+
+
+
+/*
+const fetchData = () => {
     const promise = new Promise((resolve, reject) => {
-        console.log("building Promise");
+        //console.log("building Promise");
         setTimeout(() => {
             resolve('Promise is Done!');
         }, 1500);
@@ -11,16 +39,22 @@ setTimeout(() => {
     console.log('Timer is done!');
     fetchData()
         .then(text => {
-            console.log("1:");
+            //console.log("1:");
             console.log(text);
             return fetchData();
         })
         .then(text2 => {
-            console.log("2:");
+            //console.log("2:");
             console.log(text2);
         });
 }, 2000);
 
 console.log('Hello!');
+ */
 
-
+/*
+Hello!
+Timer is done!
+Promise is Done!
+Promise is Done!
+ */
