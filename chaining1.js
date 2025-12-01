500d2f9b146e8de46abfb9507fa34842a7e0fdb5
Sol <sodanabe@gmail.com>
2025-12-01 12:56:58 +0200

    comments

--- chaining1.js
+++ chaining1.js
@@ -1,3 +1,8 @@
+/*
+Explanations:
+
+
+ */
 const fetchData = () => {
     return new Promise((resolve, reject) => {
         console.log("building Promise");
@@ -23,10 +28,6 @@ setTimeout(() => {
         });
 }, 2000);
 
-// output:
-//     Hello!
-//     Promise is Done!
-//     Promise is Done!
 
 
 // same code with async/await syntax
@@ -43,3 +44,36 @@ setTimeoutAsync(
             console.error(error);
         }
     }, 2000);
+
+/*
+output:
+Hello!
+Timer is done!
+building Promise
+Promise is Done!
+building Promise
+Promise is Done!
+
+Hello! prints immediately (synchronous code)
+2-second timer starts but doesn't block - code continues
+After 2 seconds: Timer is done! prints
+First fetchData() call:
+    building Promise prints (Promise constructor runs immediately)
+    Waits 1 second (setTimeout inside Promise)
+    Promise is Done! prints (Promise resolves)
+Second fetchData() call (from .then()):
+    building Promise prints again
+    Waits another 1 second
+    Promise is Done! prints again
+
+Total timeline:
+0s: Hello!
+2s: Timer is done!
+2s: building Promise (first)
+3s: Promise is Done! (first)
+3s: building Promise (second)
+4s: Promise is Done! (second)
+
+The operations are chained - the second fetchData() doesn't start
+until the first one completes, which is why you see the pattern twice.
+ */
